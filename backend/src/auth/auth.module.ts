0b00db3aec71b1412d997f3c174057b0f5d93944
Raul Ariel Noguera Rocha <raunogroch@MacBook-Pro.local>
2025-08-08 00:08:10 -0400

    restringiendo acceso

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { AuthController } from './auth.controller';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '60s' },
+        signOptions: { expiresIn: '60m' },
       }),
       inject: [ConfigService],
     }),
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -6,10 +6,12 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 export enum Roles {
   ATHLETE = 'athlete',
@@ -20,6 +22,7 @@ export enum Roles {
 }
 
 @Controller('users')
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
