4b0b59e57841bfc8bbe42ceae5bcd912c078656a
Raul Ariel Noguera Rocha <raunogroch@MacBook-Pro.local>
2025-08-09 21:31:23 -0400

    actualizando clubs y agregando actualizacion de coaches y athletes, ademas de evitar que se introduzcan varias veces en un solo club el mismo coach o athlete

--- backend/src/clubs/clubs.module.ts
+++ backend/src/clubs/clubs.module.ts
@@ -3,10 +3,18 @@ import { ClubsService } from './clubs.service';
 import { ClubsController } from './clubs.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Club, ClubSchema } from './schema/club.schema';
+import { UsersModule } from 'src/users/users.module';
+import { User, UserSchema } from 'src/users/schemas/user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Club.name, schema: ClubSchema }]),
+    MongooseModule.forFeature([
+      { name: Club.name, schema: ClubSchema },
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
   ],
   controllers: [ClubsController],
   providers: [ClubsService],
--- backend/src/clubs/clubs.service.ts
+++ backend/src/clubs/clubs.service.ts
@@ -4,10 +4,14 @@ import { UpdateClubDto } from './dto/update-club.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Club } from './schema/club.schema';
+import { User } from 'src/users/schemas/user.schema';
 
 @Injectable()
 export class ClubsService {
-  constructor(@InjectModel(Club.name) private clubModel: Model<Club>) {}
+  constructor(
+    @InjectModel(Club.name) private clubModel: Model<Club>,
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {}
 
   async create(createClubDto: CreateClubDto): Promise<Club> {
     const club = await this.clubModel.findOne({ name: createClubDto.name });
@@ -18,7 +22,20 @@ export class ClubsService {
   }
 
   async findAll(): Promise<Club[]> {
-    return this.clubModel.find();
+    return this.clubModel.find().populate([
+      {
+        path: 'schedule',
+        select: 'startTime endTime',
+      },
+      {
+        path: 'coaches',
+        select: 'name lastname',
+      },
+      {
+        path: 'athletes',
+        select: 'name lastname',
+      },
+    ]);
   }
 
   async findOne(id: string): Promise<Club | null> {
@@ -26,9 +43,54 @@ export class ClubsService {
   }
 
   async update(id: string, updateClubDto: UpdateClubDto): Promise<Club | null> {
-    return this.clubModel
-      .findByIdAndUpdate(id, updateClubDto, { new: true })
-      .exec();
+    const clubExist: any = await this.clubModel.findById(id);
+    if (!clubExist) throw new Error(`Club with id ${id} isn't exist`);
+
+    if (updateClubDto.coaches) {
+      const allowedCoaches: any[] = [];
+      await Promise.all(
+        updateClubDto.coaches.map(async (coach) => {
+          const user = await this.userModel.findOne({
+            _id: coach,
+            roles: 'coach',
+          });
+          if (user) {
+            const isAlreadyAdded: boolean = clubExist.coaches.includes(coach);
+            if (!isAlreadyAdded) {
+              allowedCoaches.push(coach);
+            }
+          } else {
+            console.log(`User with id ${coach} is not a coach`);
+          }
+        }),
+      );
+
+      updateClubDto.coaches = [...clubExist.coaches, ...allowedCoaches];
+    }
+
+    if (updateClubDto.athletes) {
+      const allowedAthletes: any[] = [];
+      await Promise.all(
+        updateClubDto.athletes.map(async (athlete) => {
+          const user = await this.userModel.findOne({
+            _id: athlete,
+            roles: 'athlete',
+          });
+          if (user) {
+            const isAlreadyAdded: boolean =
+              clubExist.athletes.includes(athlete);
+            if (!isAlreadyAdded) {
+              allowedAthletes.push(athlete);
+            }
+          } else {
+            console.log(`User with id ${athlete} is not a coach`);
+          }
+        }),
+      );
+
+      updateClubDto.athletes = [...clubExist.athletes, ...allowedAthletes];
+    }
+    return this.clubModel.findByIdAndUpdate(id, updateClubDto, { new: true });
   }
 
   async remove(id: string): Promise<Club | null> {
